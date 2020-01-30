1e13683ea17ea0b2c7a09770127b3f531d8178eb
unknown <predrix@gmail.com>
2020-01-30 16:59:39 +0100

    final

--- js/wrapper.js
+++
@@ -1,7 +0,0 @@
-$(window).resize(function() {
-	if ($(document).width() < 400) {
-		$('.skill').css({'flex-direction': 'column'});
-	} else {
-        $('.skill').css({'flex-direction': 'row'});
-    }
-});
